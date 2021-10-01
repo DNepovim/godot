1895d4ee6309c6a41138a363da4255a20a1e7c0f
Dominik Bláha <dominik.blaha@avast.com>
2021-10-01 17:48:40 +0200

    Add custom fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { css, Global } from "@emotion/react"
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,73 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Global styles={css`
+        @font-face {
+          font-family: 'skautbold';
+          src: url('../fonts/skaut-bold-webfont.eot');
+          src: url('../fonts/skaut-bold-webfont.eot?#iefix') format('embedded-opentype'),
+            url('../fonts/skaut-bold-webfont.woff2') format('woff2'),
+            url('../fonts/skaut-bold-webfont.woff') format('woff'),
+            url('../fonts/skaut-bold-webfont.ttf') format('truetype'),
+            url('../fonts/skaut-bold-webfont.svg#skautbold') format('svg');
+          font-weight: normal;
+          font-style: normal;
+        }
+
+        @font-face {
+          font-family: 'themix';
+          src: url('../fonts/themixc5-4_semilight-webfont.eot');
+          src: url('../fonts/themixc5-4_semilight-webfont.eot?#iefix') format('embedded-opentype'),
+            url('../fonts/themixc5-4_semilight-webfont.woff2') format('woff2'),
+            url('../fonts/themixc5-4_semilight-webfont.woff') format('woff'),
+            url('../fonts/themixc5-4_semilight-webfont.ttf') format('truetype'),
+            url('../fonts/themixc5-4_semilight-webfont.svg#themix_c5semilight') format('svg');
+          font-weight: normal;
+          font-style: normal;
+          font-display: auto;
+        }
+
+        @font-face {
+          font-family: 'themix';
+          src: url('../fonts/themixc5-4isemilightita-webfont.eot');
+          src: url('../fonts/themixc5-4isemilightita-webfont.eot?#iefix') format('embedded-opentype'),
+            url('../fonts/themixc5-4isemilightita-webfont.woff2') format('woff2'),
+            url('../fonts/themixc5-4isemilightita-webfont.woff') format('woff'),
+            url('../fonts/themixc5-4isemilightita-webfont.ttf') format('truetype'),
+            url('../fonts/themixc5-4isemilightita-webfont.svg#themix_c5semilight_italic') format('svg');
+          font-weight: normal;
+          font-style: italic;
+          font-display: auto;
+        }
+
+        @font-face {
+          font-family: 'themix';
+          src: url('../fonts/themixc5-7_bold-webfont.eot');
+          src: url('../fonts/themixc5-7_bold-webfont.eot?#iefix') format('embedded-opentype'),
+            url('../fonts/themixc5-7_bold-webfont.woff2') format('woff2'),
+            url('../fonts/themixc5-7_bold-webfont.woff') format('woff'),
+            url('../fonts/themixc5-7_bold-webfont.ttf') format('truetype'),
+            url('../fonts/themixc5-7_bold-webfont.svg#themix_c5bold') format('svg');
+          font-weight: bold;
+          font-style: normal;
+          font-display: auto;
+        }
+
+        @font-face {
+          font-family: 'themix';
+          src: url('../fonts/themixc5-7ibolditalic-webfont.eot');
+          src: url('../fonts/themixc5-7ibolditalic-webfont.eot?#iefix') format('embedded-opentype'),
+            url('../fonts/themixc5-7ibolditalic-webfont.woff2') format('woff2'),
+            url('../fonts/themixc5-7ibolditalic-webfont.woff') format('woff'),
+            url('../fonts/themixc5-7ibolditalic-webfont.ttf') format('truetype'),
+            url('../fonts/themixc5-7ibolditalic-webfont.svg#themix_c5bold_italic') format('svg');
+          font-weight: bold;
+          font-style: italic;
+          font-display: auto;
+        }
+
+
+      `} />
       <main>
       </main>
     </div>
