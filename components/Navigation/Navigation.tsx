0df0cdc2c861ed8042c0da82aec0b8ef72c65d94
Dominik Bláha <dominik.blaha@avast.com>
2021-10-05 18:17:45 +0200

    Mobile navigation

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,48 +1,80 @@
 /** @jsxImportSource @emotion/react */
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Link from "next/link"
 import Image, { ImageProps } from "next/image"
 import { theme } from "../../theme"
 import { css } from "@emotion/react"
 import { Container } from "../Container/Container"
 import styled from "@emotion/styled"
+import Hamburger from "hamburger-react"
+import { useWindowWidth } from "@react-hook/window-size"
 
 interface NavigationItem {
   title: string
   link: string
 }
 
-export const Navigation: React.FC<{logo: ImageProps["src"], data: NavigationItem[]}> = ({logo, data}) => (
-  <NavBar>
-    <Container css={css`
-      padding-top: 0;
-      padding-bottom: 0;
-    `}>
-      <Nav>
-        <Link href="/">
-          <a>
-            <Image
-              css={css`padding: 8px;`}
-              src={logo}
-              alt="Insomnia – logo"
-              width={90}
-              height={90}
-            />
-          </a>
-        </Link>
-        <NavList>
-          {data.map(item => (
-            <NavItem key={item.link}>
-              <Link href={item.link}>
-                <NavLink>{item.title}</NavLink>
-              </Link>
-            </NavItem>
-          ))}
-        </NavList>
-      </Nav>
-    </Container>
-  </NavBar>
-)
+const BREAKPOINT = 600
+
+export const Navigation: React.FC<{logo: ImageProps["src"], data: NavigationItem[]}> = ({logo, data}) => {
+  const [isOpened, setIsOpened] = useState(false)
+  const [isMobile, setIsMobile] = useState(false)
+  const width = useWindowWidth()
+
+  useEffect(() => {
+    setIsMobile(width < BREAKPOINT)
+
+
+  }, [width])
+
+  return (
+    <NavBar>
+      <Container css={css`
+        padding-top: 0;
+        padding-bottom: 0;
+      `}>
+        <Nav>
+          <Link href="/">
+            <a>
+              <Image
+                css={css`padding: 8px;`}
+                src={logo}
+                alt="Insomnia – logo"
+                width={90}
+                height={90}
+              />
+            </a>
+          </Link>
+          {!isMobile && (
+            <NavList>
+              {data.map(item => (
+                <NavItem key={item.link}>
+                  <Link href={item.link}>
+                    <NavLink>{item.title}</NavLink>
+                  </Link>
+                </NavItem>
+              ))}
+            </NavList>
+            )
+          }
+          {isMobile && (
+            <NavListMobile isOpened={isOpened}>
+              {data.map(item => (
+                <NavItem key={item.link}>
+                  <Link href={item.link}>
+                    <NavLinkMobile>{item.title}</NavLinkMobile>
+                  </Link>
+                </NavItem>
+              ))}
+            </NavListMobile>
+            )
+          }
+          {isMobile && <Hamburger color={theme.color.brand} onToggle={() => setIsOpened(!isOpened)} />}
+        </Nav>
+      </Container>
+    </NavBar>
+  )
+}
 
 const NavBar = styled("div")`
   position: fixed;
@@ -74,10 +106,8 @@ const NavItem = styled("li")`
 const NavLink = styled("a")`
   position: relative;
   display: block;
-  text-decoration: none;
   padding: 0.4rem;
   transition: color 300ms;
-  font-family: themix;
   color: black;
   cursor: pointer;
 
@@ -102,4 +132,22 @@ const NavLink = styled("a")`
       width: 100%;
     }
   }
+`
+
+const NavListMobile = styled.ul((props: { isOpened: boolean }) => `
+  background-color: white;
+  position: absolute;
+  top: 100%;
+  right: 0;
+  list-style: none;
+  margin: 0;
+  padding: 0 0 4px;
+  transform: translateX(${props.isOpened ? 0 : 100}%);
+  transition: transform 200ms;
+`)
+
+const NavLinkMobile = styled("a")`
+  display: block;
+  color: black;
+  padding: 0.6em 1.6em;
 `
