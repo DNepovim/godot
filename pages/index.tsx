1c2c9e78966f6f21f45cfbc531570e82bfd1e5ce
Dominik Bláha <dominik.blaha@avast.com>
2021-10-06 16:45:46 +0200

    Separate global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,9 @@ import { Navigation as NavigationType, Page } from "../data"
 import { Navigation } from "../components/Navigation/Navigation"
 import logo from "../images/logo.png"
 import { css, Global } from "@emotion/react"
-import { theme } from "../theme"
 import { blocks } from "../blocks/blocks"
 import { getNavigation, getPage } from "../firebase/firebase"
+import { globalStyles } from "../globalStyles"
 
 const Home: NextPage<Props> = ({navigation, page}) => (
   <div>
@@ -17,93 +17,7 @@ const Home: NextPage<Props> = ({navigation, page}) => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <Global styles={css`
-      @font-face {
-        font-family: 'skautbold';
-        src: url('../fonts/skaut-bold-webfont.eot');
-        src: url('../fonts/skaut-bold-webfont.eot?#iefix') format('embedded-opentype'),
-          url('../fonts/skaut-bold-webfont.woff2') format('woff2'),
-          url('../fonts/skaut-bold-webfont.woff') format('woff'),
-          url('../fonts/skaut-bold-webfont.ttf') format('truetype'),
-          url('../fonts/skaut-bold-webfont.svg#skautbold') format('svg');
-        font-weight: normal;
-        font-style: normal;
-      }
-
-      @font-face {
-        font-family: 'themix';
-        src: url('../fonts/themixc5-4_semilight-webfont.eot');
-        src: url('../fonts/themixc5-4_semilight-webfont.eot?#iefix') format('embedded-opentype'),
-          url('../fonts/themixc5-4_semilight-webfont.woff2') format('woff2'),
-          url('../fonts/themixc5-4_semilight-webfont.woff') format('woff'),
-          url('../fonts/themixc5-4_semilight-webfont.ttf') format('truetype'),
-          url('../fonts/themixc5-4_semilight-webfont.svg#themix_c5semilight') format('svg');
-        font-weight: normal;
-        font-style: normal;
-        font-display: auto;
-      }
-
-      @font-face {
-        font-family: 'themix';
-        src: url('../fonts/themixc5-4isemilightita-webfont.eot');
-        src: url('../fonts/themixc5-4isemilightita-webfont.eot?#iefix') format('embedded-opentype'),
-          url('../fonts/themixc5-4isemilightita-webfont.woff2') format('woff2'),
-          url('../fonts/themixc5-4isemilightita-webfont.woff') format('woff'),
-          url('../fonts/themixc5-4isemilightita-webfont.ttf') format('truetype'),
-          url('../fonts/themixc5-4isemilightita-webfont.svg#themix_c5semilight_italic') format('svg');
-        font-weight: normal;
-        font-style: italic;
-        font-display: auto;
-      }
-
-      @font-face {
-        font-family: 'themix';
-        src: url('../fonts/themixc5-7_bold-webfont.eot');
-        src: url('../fonts/themixc5-7_bold-webfont.eot?#iefix') format('embedded-opentype'),
-          url('../fonts/themixc5-7_bold-webfont.woff2') format('woff2'),
-          url('../fonts/themixc5-7_bold-webfont.woff') format('woff'),
-          url('../fonts/themixc5-7_bold-webfont.ttf') format('truetype'),
-          url('../fonts/themixc5-7_bold-webfont.svg#themix_c5bold') format('svg');
-        font-weight: bold;
-        font-style: normal;
-        font-display: auto;
-      }
-
-      @font-face {
-        font-family: 'themix';
-        src: url('../fonts/themixc5-7ibolditalic-webfont.eot');
-        src: url('../fonts/themixc5-7ibolditalic-webfont.eot?#iefix') format('embedded-opentype'),
-          url('../fonts/themixc5-7ibolditalic-webfont.woff2') format('woff2'),
-          url('../fonts/themixc5-7ibolditalic-webfont.woff') format('woff'),
-          url('../fonts/themixc5-7ibolditalic-webfont.ttf') format('truetype'),
-          url('../fonts/themixc5-7ibolditalic-webfont.svg#themix_c5bold_italic') format('svg');
-        font-weight: bold;
-        font-style: italic;
-        font-display: auto;
-      }
-
-      body {
-        margin: 0;
-        padding: 0;
-        font-size: 18px;
-        font-family: themix;
-      }
-
-      a {
-        color: ${theme.color.brand};
-        text-decoration: none;
-
-        &:hover {
-          text-decoration: underline;
-        }
-      }
-
-      ::selection {
-        background-color: ${theme.color.brand};
-      }
-
-
-    `} />
+    <Global styles={globalStyles} />
 
     <main css={css`
       min-height: 300vh;
