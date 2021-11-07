d605b3efbe4f9218c5cadb471a82e209d2ab1109
Dominik Bláha <dominik.blaha@avast.com>
2021-11-08 00:12:20 +0100

    Change favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,15 @@ const Home: NextPage<Props> = ({navigation, page}) => (
   <div>
     <Head>
       <title>ČLK Insomnia</title>
-      <link rel="icon" href="/favicon.ico" />
+
+      <link rel="icon" href="favicons/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#9dcfc3" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
 
       <link rel="preconnect" href="https://cdn.skauting.cz" />
       <link href="https://cdn.skauting.cz/fonts/fonts.css" rel="stylesheet" />
