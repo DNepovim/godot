7595afbbb7d90eaaf4a0816838b335f5d3d237e2
Dominik Bláha <dominik.blaha@avast.com>
2021-12-01 17:44:35 +0100

    Create admin layout

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -9,10 +9,10 @@ import { blockDefs, BlocksDefs } from "../../blocks/blocks"
 import { auth, logout, login } from "../../firebase/auth"
 import { Spinner } from '../../admin/components/Spinner/Spinner'
 import { Centered } from '../../admin/components/Centered/Centered'
-import { Avatar, Button, Layout, message, PageHeader } from 'antd'
+import { Avatar, Breadcrumb, Button, Layout, Menu, message, PageHeader } from 'antd'
 import 'antd/dist/antd.css'
 import { AppstoreAddOutlined, SaveOutlined, LogoutOutlined, LoginOutlined } from '@ant-design/icons'
-import { Header } from 'antd/lib/layout/layout'
+import { Content, Header } from 'antd/lib/layout/layout'
 import { css } from '@emotion/react'
 import { arrayMove, SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy } from '@dnd-kit/sortable'
 import { useSensors, useSensor, PointerSensor, KeyboardSensor, closestCenter, DndContext } from '@dnd-kit/core'
@@ -20,6 +20,12 @@ import { v4 as uuid } from 'uuid'
 import { Overwrite } from 'utility-types/dist/mapped-types'
 import { enumToSchemaOptions } from "../../admin/utils/enumToSchemaOptions"
 import { BlockTemplates } from "../../blocks/blockTemplates"
+import Sider from "antd/lib/layout/Sider"
+import React from "react"
+import FileOutlined from "@ant-design/icons/lib/icons/FileOutlined"
+import MenuOutlined from "@ant-design/icons/lib/icons/MenuOutlined"
+import TeamOutlined from "@ant-design/icons/lib/icons/TeamOutlined"
+import ToolOutlined from "@ant-design/icons/lib/icons/ToolOutlined"
 
 type Blocks = Overwrite<Partial<BlocksDefs>, Pick<BlocksDefs, "id">>[]
 
@@ -45,73 +51,82 @@ const Admin: NextPage<Props> = ({blocks}) => {
 
   return (
     <Layout css={css`min-height: 100vh`}>
-      <Header css={css`display: flex; align-items: center; justify-content: flex-end; `}>
-          <Button onClick={logout} icon={<LogoutOutlined />} type="link">Odhlásit</Button>
-          <Avatar alt={user.displayName} src={user.photoURL}/>
+      <Header css={css`display: flex; align-items: center; justify-content: flex-end; `} >
+        <Button onClick={logout} icon={<LogoutOutlined />} type="link">Odhlásit</Button>
+        <Avatar alt={user.displayName} src={user.photoURL}/>
       </Header>
-      <Formik<Blocks>
-        onSubmit={async (values: Blocks) => {
-          try {
-            await values.forEach(async (block, index) => {
-              await updateBlock("frontPage", index, block)
-            })
-          } catch (e) {
-            console.error(e)
-            message.error("Vyskytl se nějaký problém. Zkuste to znovu.")
-          }
-          message.success("Stránka byla uložena.")
-        }}
-        validationSchema={() => yup.lazy((values: Blocks) => yup.array().of(yup.object().shape({
-          template: yup.string().oneOf(enumToSchemaOptions(BlockTemplates)).required(),
-          fields: yup.mixed().when("template", (template: BlockTemplates) => template ? blockDefs[template].schema : yup.mixed())
-        })))}
-        initialValues={blocks}
-      >
-        {props => (
-          <PageHeader
-            title="Hlavní stránka"
-            extra={<Button type="primary" icon={<SaveOutlined />} onClick={async () => props.submitForm()} disabled={props.isSubmitting} loading={props.isSubmitting}>Uložit</Button>}
-            footer={<Button icon={<AppstoreAddOutlined />} onClick={() => props.setValues([...props.values, {id: uuid()}])}>Přidat blok</Button>}
-          >
-            <Form>
-              <DndContext
-                sensors={sensors}
-                collisionDetection={closestCenter}
-                onDragEnd={(event) => {
-                  const {active, over} = event
-
-                  if (!over || active.id === over.id) {
-                    return
-                  }
-
-                  const items = props.values.map(v => v.id)
-                  const overIndex = items.indexOf(over.id)
-                  const activeIndex = items.indexOf(active.id)
-                  const newOrder = arrayMove(props.values, activeIndex, overIndex)
-
-                  props.setValues(newOrder)
-                }}
-              >
-                <SortableContext
-                  items={props.values.map(v => v.id)}
-                  strategy={verticalListSortingStrategy}
+      <Layout>
+        <Layout.Sider width={200} css={css`background-color: white`}>
+          <Menu>
+            <Menu.Item icon={<FileOutlined />}>Stránky</Menu.Item>
+            <Menu.Item icon={<MenuOutlined />}>Navigace</Menu.Item>
+            <Menu.Item icon={<TeamOutlined />}>Uživatelé</Menu.Item>
+            <Menu.Item icon={<ToolOutlined />}>Nastavení</Menu.Item>
+          </Menu>
+        </Layout.Sider>
+        <Layout>
+          <Content>
+            <Formik<Blocks>
+              onSubmit={async (values: Blocks) =>
+                values.forEach(async (block, index) => {
+                  await updateBlock("frontPage", index, block)
+                })
+              }
+              validationSchema={() => yup.lazy((values: Blocks) => yup.array().of(yup.object().shape({
+                template: yup.string().oneOf(enumToSchemaOptions(BlockTemplates)).required(),
+                fields: yup.mixed().when("template", (template: BlockTemplates) => template ? blockDefs[template].schema : yup.mixed())
+              })))}
+              initialValues={blocks}
+            >
+              {props => (
+                <PageHeader
+                  title="Hlavní stránka"
+                  breadcrumb={{routes:[{breadcrumbName: "Stránky", path: ""}, {breadcrumbName: "Hlavní stránka", path: ""}]}}
+                  extra={<Button type="primary" icon={<SaveOutlined />} onClick={async () => props.submitForm()} disabled={props.isSubmitting} loading={props.isSubmitting}>Uložit</Button>}
+                  footer={<Button icon={<AppstoreAddOutlined />} onClick={() => props.setValues([...props.values, {id: uuid()}])}>Přidat blok</Button>}
                 >
-                  {props.values.map((block, index) => (
-                    <SortableAdminBlockFields
-                      key={block.id}
-                      index={index}
-                      id={block.id}
-                      {...(block.template ? blockDefs[block.template] : {})}
-                      onRemove={() => props.setValues(props.values.filter((_, i) => i !== index))}
-                      onTemplateChange={template => props.setFieldValue(`[${index}].template`, template)}
-                    />
-                  ))}
-                </SortableContext>
-              </DndContext>
-            </Form>
-          </PageHeader>
-        )}
-      </Formik>
+                  <Form>
+                    <DndContext
+                      sensors={sensors}
+                      collisionDetection={closestCenter}
+                      onDragEnd={(event) => {
+                        const {active, over} = event
+
+                        if (!over || active.id === over.id) {
+                          return
+                        }
+
+                        const items = props.values.map(v => v.id)
+                        const overIndex = items.indexOf(over.id)
+                        const activeIndex = items.indexOf(active.id)
+                        const newOrder = arrayMove(props.values, activeIndex, overIndex)
+
+                        props.setValues(newOrder)
+                      }}
+                    >
+                      <SortableContext
+                        items={props.values.map(v => v.id)}
+                        strategy={verticalListSortingStrategy}
+                      >
+                        {props.values.map((block, index) => (
+                          <SortableAdminBlockFields
+                            key={block.id}
+                            index={index}
+                            id={block.id}
+                            {...(block.template ? blockDefs[block.template] : {})}
+                            onRemove={() => props.setValues(props.values.filter((_, i) => i !== index))}
+                            onTemplateChange={template => props.setFieldValue(`[${index}].template`, template)}
+                          />
+                        ))}
+                      </SortableContext>
+                    </DndContext>
+                  </Form>
+                </PageHeader>
+              )}
+            </Formik>
+          </Content>
+        </Layout>
+      </Layout>
     </Layout>
   )
 }
