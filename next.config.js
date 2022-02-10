686c94e1d9ef5fb0cc1d851dd142ad406223f917
Dominik Bláha <dominik.blaha@avast.com>
2022-02-10 13:55:39 +0100

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -10,3 +10,8 @@ module.exports = {
     ]
   },
 }
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+module.exports = withBundleAnalyzer({})
