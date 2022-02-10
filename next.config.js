571817517068128892ecf6d732684dc6bfca6f61
Dominik Bláha <dominik.blaha@avast.com>
2022-02-10 13:55:52 +0100

    Add lang attribute to html

--- next.config.js
+++ next.config.js
@@ -15,3 +15,10 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 })
 module.exports = withBundleAnalyzer({})
+
+module.exports = {
+  i18n: {
+    locales: ["cs"],
+    defaultLocale: "cs",
+  },
+}
