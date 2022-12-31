0b8db56e247037783f3c9a4d235bb9e4a8fa0a1a
Dominik Bláha <dominik.blaha@avast.com>
2022-12-31 16:41:56 +0100

    Specify storage bucket

--- src/firebase/storage.ts
+++ src/firebase/storage.ts
@@ -1,4 +1,7 @@
 import { getStorage } from "firebase/storage"
 import { firebaseApp } from "./firebase"
 
-export const storage = getStorage(firebaseApp)
+export const storage = getStorage(
+  firebaseApp,
+  `gs://${process.env.GATSBY_PUBLIC_FIREBASE_STORAGE_BUCKET}`
+)
